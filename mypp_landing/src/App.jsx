e501c3d54ace52ae89982f30552edbb49010eb9e
boredom1234 <roughuse82@gmail.com>
2023-10-10 01:06:42 +0530

    --

--- mypp_landing/src/App.jsx
+++ mypp_landing/src/App.jsx
@@ -6,23 +6,33 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
-        }}
-      >
+      <div className="hero min-h-screen relative">
+        <video autoPlay loop muted className="video-bg absolute top-0 left-0 min-w-full min-h-full">
+          <source src="https://i.imgur.com/6o1Uc6H.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">
+              Hello there &nbsp;
+              <span className="loading loading-ring loading-md"></span>
+            </h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              A to-do list app is a digital tool designed for systematic task
+              organization and management. With customizable features, it
+              enables users to set deadlines, categorize tasks, and establish
+              priorities, fostering enhanced productivity and goal achievement
+              in both professional and personal contexts.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <div className="join">
+              <a href="https://www.google.com">
+                <button className="btn btn-primary join-item">Sign Up</button>
+              </a>
+              <a href="https://www.google.com">
+                <button className="btn btn-neutral join-item">LOGIN</button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
